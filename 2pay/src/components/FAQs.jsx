bd599330e637a295d7351a4cbeebfb29d48d8a2d
Victor Malomo <malomovictor@gmail.com>
2025-05-17 07:34:43 +0100

    fixed FAQs bug

--- 2pay/src/components/FAQs.jsx
+++ 2pay/src/components/FAQs.jsx
@@ -86,35 +86,11 @@ const Faqs = () => {
           </div>
         </div>
 
-
         {showArrow && (
           <button className="scroll-arrow" onClick={scrollRight}>
             ➜
           </button>
         )}
-
-        <div className="card-3 box card">
-          <div className="card__heading">Can I cancel my contribution?</div>
-          <p className="card__body">
-            Due to the nature of blockchain transactions, contributions are
-            typically irreversible. Contact our support team for guidance{" "}
-          </p>
-        </div>
-        <div className="card-3 box card">
-          <div className="card__heading">Can I cancel my contribution?</div>
-          <p className="card__body">
-            Due to the nature of blockchain transactions, contributions are
-            typically irreversible. Contact our support team for guidance{" "}
-          </p>
-        </div>
-        <div className="card-3 box card">
-          <div className="card__heading">Can I cancel my contribution?</div>
-          <p className="card__body">
-            Due to the nature of blockchain transactions, contributions are
-            typically irreversible. Contact our support team for guidance{" "}
-          </p>
-        </div>
-
       </div>
     </div>
   );
